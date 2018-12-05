fae584419c723977c8fa5bc48cf89e7ad1453cf2
Alixander Roden <32658395+S190762@users.noreply.github.com>
2018-12-05 06:06:19 +0000

    Upload of unity Prototype 3

---
+++ Game Prototype/Prototype 3/Assets/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    bool collision = false;
+
+    bool collected = false;
+
+    void Update()
+    {
+
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!collision)
+        {
+            collision = true;
+        }
+    }
+}
